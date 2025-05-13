dd3b3d8215ac070e76f1e9eddf4191d9dc90c966
Felipe Menezes <168611944+Fehlipe19@users.noreply.github.com>
2025-05-12 21:52:55 -0400

    first commit

---
+++ tire_volume.py
@@ -0,0 +1,10 @@
+import math
+
+width_mm = float(input("Enter the width of the tire in mm: "))
+aspect_ratio = float(input("Enter the aspect ratio of the tire: "))
+diameter_inches = float(input("Enter the diameter of the tire in inches: "))
+
+tire_volume_liters = math.pi * width_mm ** 2 * aspect_ratio * (width_mm * aspect_ratio + 2540 * diameter_inches) / 10000000000
+
+print(f"The volume of the tire is approximately {tire_volume_liters:.2f} liters.")
+
