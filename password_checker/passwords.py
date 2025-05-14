08c8a808923a87acbdc789fb358365f324c1d61f
Felipe Menezes <168611944+Fehlipe19@users.noreply.github.com>
2025-05-13 21:27:37 -0400

    Call functions to check strength inside main

--- password_checker/passwords.py
+++ password_checker/passwords.py
@@ -7,8 +7,35 @@ def main():
     """
     The main function of the program.
     """
+    
+    stop = "a"
+
+    while stop != "quit":
+        stop = input("Enter 'quit' to exit or 'continue' to check another password: ").strip().lower()
+        if stop == "continue":
+            password = input("Enter a password to check: ").strip()
+            word_in_file(password, "toppasswords.txt", True)
+            word_has_character(password, LOWER)
+            word_has_character(password, UPPER)
+            word_has_character(password, DIGITS)
+            word_has_character(password, SPECIAL)
+            word_complexity(password)
+            password_strength(password, 10, 15)
+
+        elif stop == "quit":
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid input. Please enter 'quit' or 'continue'.")
+    
 
 def word_in_file(word, filename, case_sensitive):
+    """
+    Check if password is a common password.
+    """
+    with open("passwords.txt", "r", encoding="utf-8") as password_file:
+        passwords = password_file.readlines()
+
     pass
 
 def word_has_character(word, character_list):
@@ -20,8 +47,6 @@ def word_complexity(word):
 def password_strength(password, min_length, strong_length):
     pass
 
-
-main()
 
 if __name__ == "__main__":
     main()
