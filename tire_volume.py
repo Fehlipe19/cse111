a83b2fcd91e976a3b642fd77d5802b43ad3d0e41
Felipe Menezes <168611944+Fehlipe19@users.noreply.github.com>
2025-05-13 20:32:46 -0400

    Edited calculations

--- tire_volume.py
+++ tire_volume.py
@@ -1,3 +1,5 @@
+#To enhance my program, I added a feature that show the user hwo much the tire costs, and a feature that allows the user to input their phone number if they decide to buy the tire.
+
 import math
 from datetime import datetime
 
@@ -9,7 +11,7 @@ diameter_inches = float(input("Enter the diameter of the tire in inches: "))
 
 tire_volume_liters = math.pi * width_mm ** 2 * aspect_ratio * (width_mm * aspect_ratio + 2540 * diameter_inches) / 10000000000
 
-# print(f"The volume of the tire is approximately {tire_volume_liters:.2f} liters.")
+print(f"\nThe volume of the tire is approximately {tire_volume_liters:.2f} liters.\n")
 
 full_date = datetime.now()
 tire_price = 0
@@ -23,7 +25,7 @@ elif 225 <= width_mm < 245:
 elif width_mm >=245:
     tire_price = 145.99
 
-will_buy = input(f" The price of the tire is ${tire_price:.2f}. Will you buy this tire? (yes/no): ").strip().lower()
+will_buy = input(f"The price of the tire is ${tire_price:.2f}. Will you buy this tire? (yes/no): ").strip().lower()
 if will_buy == "yes":
     customer_phone_number = input("Please enter your phone number: ")
     with open("volumes.txt", "a") as file:
